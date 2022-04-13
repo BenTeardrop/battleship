84e400153b2ba9834603d77a6fea345e2bb77276
Ben Jim <jimenezbenjamin@yahoo.com>
2022-04-13 10:42:30 +0000

    Generate 2 random coordinates one for rows and one for column

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
+import random
+
+
 board = [('0') * 5 for x in range(5)]
 computer_board = [('0') * 5 for x in range(5)]
 
 
 def print_board(board):
     """
-    Prints the board and an horinzontal line 
+    Prints the board 5 x 5 coordinates
+    and an horinzontal line 
     to separate the two boards
     """
     print ('---------')
@@ -13,4 +17,17 @@ def print_board(board):
     print ('---------')
 
 
+def generate_random_coords():
+    """
+    Generates 2 random coordinates integers
+    1 for row and 1 for column
+    """
+    row = int(random.randint(1, 5))
+    column = int(random.randint(1, 5))
+    print(f'random row:{row} and column: {column}')
+    return (row, column)
+
+
+generate_random_coords()
 print_board(board)
+print_board(computer_board)
