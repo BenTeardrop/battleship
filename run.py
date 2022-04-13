94adb39cee9d09ad8974551a59f67f0a4fcb3a1d
Ben Jim <jimenezbenjamin@yahoo.com>
2022-04-13 10:29:48 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = [('0') * 5 for x in range(5)]
+computer_board = [('0') * 5 for x in range(5)]
+
+
+def print_board(board):
+    """
+    Prints the board and an horinzontal line 
+    to separate the two boards
+    """
+    print ('---------')
+    for row in board:
+        print(" ".join(row))
+    print ('---------')
+
+
+print_board(board)
